603d674c15cf6a66152292cda505bbb5e923cf8b
Amirmbz <164421263+Amirmbz@users.noreply.github.com>
2024-09-26 13:03:06 +0330

    statics

--- Ecommerce/home/urls.py
+++ Ecommerce/home/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('/cart', views.cart, name='cart'),
+    path('/about', views.about, name='about'),
+    path('/shop', views.shop, name='shop'),
+    path('/contact', views.contact, name='contact'),
+    path('', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('', views.index, name='index'),
 ]
--- Ecommerce/home/views.py
+++ Ecommerce/home/views.py
@@ -4,3 +4,39 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'home/index.html')
+
+
+def cart(request):
+    return render(request, 'home/cart.html')
+
+
+def about(request):
+    return render(request, 'home/about.html')
+
+
+def shop(request):
+    return render(request, 'home/shop.html')
+
+
+def contact(request):
+    return render(request, 'home/contact.html')
+
+
+def index(request):
+    return render(request, 'home/index.html')
+
+
+def index(request):
+    return render(request, 'home/index.html')
+
+
+def index(request):
+    return render(request, 'home/index.html')
+
+
+def index(request):
+    return render(request, 'home/index.html')
+
+
+def index(request):
+    return render(request, 'home/index.html')
