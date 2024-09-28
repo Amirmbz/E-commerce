d55bef78e60e0776f23cf9ea11a2766e2a4f6fcf
Amirmbz <164421263+Amirmbz@users.noreply.github.com>
2024-09-28 09:00:54 +0330

    index

--- Ecommerce/home/urls.py
+++ Ecommerce/home/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/cart', views.cart, name='cart'),
-    path('/about', views.about, name='about'),
-    path('/shop', views.shop, name='shop'),
-    path('/contact', views.contact, name='contact')
+    path('cart/', views.cart, name='cart'),
+    path('about/', views.about, name='about'),
+    path('shop/', views.shop, name='shop'),
+    path('contact/', views.contact, name='contact')
 ]
